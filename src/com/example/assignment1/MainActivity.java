0b95e832db4026a9519db203503f8fdd5704931b
morganpatz <patzelt@ualberta.ca>
2014-09-25 11:35:37 -0600

    first commit

--- src/com/example/assignment1/MainActivity.java
+++ src/com/example/assignment1/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.assignment1;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -16,6 +19,20 @@ public class MainActivity extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
+		
+
+		Button todo = (Button) findViewById(R.id.btn_todo);
+
+		todo.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View v) {
+				Intent myIntent = new Intent(MainActivity.this,
+						ToDo.class);
+				startActivity(myIntent);
+
+			}
+		});
+		
 		return true;
 	}
 
--- src/com/example/assignment1/ToDo.java
+++ src/com/example/assignment1/ToDo.java
@@ -1,22 +1,178 @@
 package com.example.assignment1;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+
+ 
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
+ 
+public class ToDo extends Activity
+{
+ 
+          MyCustomAdapter dataAdapter = null;
+ 
+            @Override
+            public void onCreate(Bundle savedInstanceState)
+            {
+               super.onCreate(savedInstanceState);
+               setContentView(R.layout.activity_todo);
+ 
+               //Generate list View from ArrayList
+               displayListView();
+ 
+               checkButtonClick();
+ 
+            }
+ 
+            private void displayListView()
+            {
+            		
+            
+                    //Array list of countries
+                    ArrayList<Task> taskList = new ArrayList<Task>();
+ 
+                    Task tasks = new Task("First todo",false);
+                    taskList.add(tasks);
+                   
+ 
+                    //create an ArrayAdaptar from the String Array
+                    dataAdapter = new MyCustomAdapter(this,R.layout.task_info, taskList);
+                    ListView listView = (ListView) findViewById(R.id.listView1);
+                    // Assign adapter to ListView
+                    listView.setAdapter(dataAdapter);
+ 
+                    listView.setOnItemClickListener(new OnItemClickListener()
+                    {
+ 
+                            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+                            {
+                                    // When clicked, show a toast with the TextView text
+                                    Task task = (Task) parent.getItemAtPosition(position);
+                                    Toast.makeText(getApplicationContext(),"Clicked on Row: " + task.getName(),
+                                    Toast.LENGTH_LONG).show();
+                            }
+                    });
+            }
+            
+            public class MyCustomAdapter extends ArrayAdapter<Task> {
 
-public class ToDo extends Activity {
+            	private ArrayList<Task> taskList;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_todo);
-	}
+            	public MyCustomAdapter(Context context, int textViewResourceId,
+            			ArrayList<Task> taskList) {
+            		super(context, textViewResourceId, taskList);
+            		this.taskList = new ArrayList<Task>();
+            		this.taskList.addAll(taskList);
+            	}
+            
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.to_do, menu);
-		return true;
-	}
+            	private class ViewHolder {
+            		CheckBox name;
+            	}
 
+            	@Override
+            	public View getView(int position, View convertView, ViewGroup parent) {
+
+            		ViewHolder holder = null;
+
+            		Log.v("ConvertView", String.valueOf(position));
+
+            		if (convertView == null) {
+
+            			LayoutInflater vi = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            			convertView = vi.inflate(R.layout.task_info, null);
+
+            			holder = new ViewHolder();
+            			holder.name = (CheckBox) convertView.findViewById(R.id.checkBox1);
+
+            			convertView.setTag(holder);
+
+            			holder.name.setOnClickListener(new View.OnClickListener() {
+            				public void onClick(View v) {
+            					CheckBox cb = (CheckBox) v;
+            					Task tasks = (Task) cb.getTag();
+
+            					Toast.makeText(
+            							getApplicationContext(),
+            							"Clicked on Checkbox: " + cb.getText() + " is "
+            									+ cb.isChecked(), Toast.LENGTH_LONG).show();
+
+            					tasks.setSelected(cb.isChecked());
+            				}
+            			});
+
+            		} else {
+            			holder = (ViewHolder) convertView.getTag();
+            		}
+
+            		Task task = taskList.get(position);
+
+            		holder.name.setText(task.getName());
+            		holder.name.setChecked(task.isSelected());
+
+            		holder.name.setTag(task);
+
+            		return convertView;
+            	}
+
+            	
+
+            }
+            
+            private void checkButtonClick()
+            {
+     
+                    ImageButton myButton = (ImageButton) findViewById(R.id.btn_check);
+     
+                    myButton.setOnClickListener(new OnClickListener()
+                    {
+     
+                                @Override
+                                public void onClick(View v)
+                                {
+     
+                                                StringBuffer responseText = new StringBuffer();
+                                                responseText.append("The following were selected...\n");
+     
+                                                ArrayList<Task> taskList = dataAdapter.taskList;
+     
+                                                for(int i=0;i<taskList.size();i++)
+                                                {
+                                                     Task task = taskList.get(i);
+     
+                                                    if(task.isSelected())
+                                                    {
+                                                        responseText.append("\n" + task.getName());
+                                                    }
+                                                }
+     
+                                                Toast.makeText(getApplicationContext(),
+                                                responseText, Toast.LENGTH_LONG).show();
+                                }
+                    });
+            }
+            
+            private void newTask() {
+            	
+            }
+            
+        
 }
+
+
